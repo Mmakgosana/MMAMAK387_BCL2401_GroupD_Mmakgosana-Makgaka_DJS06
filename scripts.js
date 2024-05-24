b0cb8ef147a9d69ecd51c7bef85084007e8f8918
Mmakgosana <makgakammakgosana@gmail.com>
2024-05-24 13:38:35 +0200

    I have corrected a syntax

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
-// A list of provinces:
-const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State']
+// List of provinces
+const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State'];
 
-// A list of names:
-const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie']
+// List of names
+const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie'];
 
-// A list of products with prices:
+// List of products with prices
 const products = [
   { product: 'banana', price: "2" },
   { product: 'mango', price: 6 },
@@ -12,48 +12,57 @@ const products = [
   { product: 'avocado', price: "8" },
   { product: 'coffee', price: 10 },
   { product: 'tea', price: '' },
-]
+];
 
-//Logs each name
+
+// Logs each name
 names.forEach(name => console.log(name));
 
-//Logs each province
+// Logs each province
 provinces.forEach(province => console.log(province));
 
-// Create a new array of province names in all Uppercase
+// Log each name with its matching province
+names.forEach((name, index) => {
+  console.log(`${name} (${provinces[index]})`);
+});
+
+// Create a new array of province names in all uppercase
 const uppercaseProvinces = provinces.map(province => province.toUpperCase());
 console.log(uppercaseProvinces);
 
-// Create a new array that contains the length of each name
+// CreateS a new array that contains the length of each name
 const nameLengths = names.map(name => name.length);
 console.log(nameLengths);
+
 
 // Alphabetically sort the provinces
 const sortedProvinces = [...provinces].sort();
 console.log(sortedProvinces);
 
-// Remove provinces containing "Cape"  and log the count of remaining provinces
+
+// Remove provinces containing "Cape" and log the count of remaining provinces
 const filteredProvinces = provinces.filter(province => !province.includes('Cape'));
 console.log(filteredProvinces.length);
 
-// Create a boolean array to determine if a name contains the letter 'S'
-const containsS = names.map(name => name.includes ('S'));
+// Creates a boolean array to determine if a name contains the letter 'S'
+const containsS = names.map(name => name.includes('S'));
 console.log(containsS);
 
 // Transform the names array into an object mapping names to their respective provinces
 const nameToProvinceMap = names.reduce((obj, name, index) => {
-  obd[name] = provinces[index];
+  obj[name] = provinces[index];
   return obj;
-}, {};
+}, {});
 console.log(nameToProvinceMap);
 
+
 // Log each name and province with matching format "Name (Province)"
-console.log(names.map((name, index) => `${name}(${provinces[index]})`));
+console.log(names.map((name, index) => `${name} (${provinces[index]})`));
 
-//Logs the new array of province names in all uppercaseProvinces
-console.log(provinces.map(provinces => province.toUpperCase()));
+// Logs the new array of province names in all uppercase
+console.log(provinces.map(province => province.toUpperCase()));
 
-//Logs the array of name lengths
+// Logs the array of name lengths
 console.log(names.map(name => name.length));
 
 // Logs the alphabetically sorted provinces
@@ -70,5 +79,3 @@ console.log(names.reduce((obj, name, index) => {
   obj[name] = provinces[index];
   return obj;
 }, {}));
-
-)
